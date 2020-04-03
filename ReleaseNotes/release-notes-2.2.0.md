00edfa7880a9a67a86e5b6b461ecad60efeccf93
shiruiwei <shiruiwei@qiniu.com>
2020-04-03 10:47:53 +0800

    Update release file and bin

---
+++ ReleaseNotes/release-notes-2.2.0.md
@@ -0,0 +1,27 @@
+# QNRTCWin Release Notes for 2.2.0
+
+## 简介
+QNRTCWin 是七牛云推出的一款适用于 Windows7 及以上系统平台的实时音视频 SDK，提供了包含音视频通话、屏幕分享、数据导入、监听等功能，适用于各种直播、教育、医疗等场景，并提供了灵活的开发接口，支持高度定制以及二次开发。
+
+## 注意
+
+自 2.0.0 后，我们为了提高用户阅读文档的体验，使用了新的文档站（老文档地址继续保留），新文档站地址 https://doc.qnsdk.com/rtn
+
+体验新版 Demo 请打开 Demo 后点击 “体验新版本（多Track）” 即可体验
+
+## 功能
+- 新增创建自定义合流任务
+- 增加内部数据打点
+
+## 优化
+
+## 缺陷
+
+## 注意事项
+- 2.0.0 以前的接口文件移动到了 qiniu 目录下
+- 2.1.0 以后不再提供 Debug 版本的动态库
+- [Demo 源码 GitHub 地址](https://github.com/pili-engineering/QNRTC-Windows)
+- [Demo 体验下载地址](https://sdk-release.qnsdk.com/WindowsDemo.zip) 
+
+## 问题反馈 
+当你遇到任何问题时，可以通过在 GitHub 的 repo 提交 `issues` 来反馈问题，请尽可能的描述清楚遇到的问题，如果有错误信息也一同附带，并且在 ```Labels``` 中指明类型为 bug 或者其他。 [通过这里查看已有的 issues 和提交 bug](https://github.com/pili-engineering/QNRTC-Windows)
---
+++ RtcDemo/MergeDialog.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "stdafx.h"
+#include "resource.h"
+#include "afxdialogex.h"
+#include <string>
+
+// MergeDialog dialog
+
+class MergeDialog : public CDialogEx
+{
+	DECLARE_DYNAMIC(MergeDialog)
+
+public:
+	MergeDialog(CWnd* pParent = nullptr);   // standard constructor
+	virtual ~MergeDialog();
+
+    afx_msg void OnBnClickedButtonMergeOk();
+    afx_msg void OnBnClickedButtonMergeCancel();
+
+    struct MergeConfig
+    {
+        std::string publish_url     = "rtmp://pili-publish.live.zhangrui.qiniuts.com/live-rui/flow-2";
+
+        int32_t job_width           = 480;
+        int32_t job_height          = 480;
+        int32_t job_fps             = 20;;
+        int32_t job_bitrate         = 800 * 1000;
+        int32_t job_max_bitrate     = 1000 * 1000;
+        int32_t job_min_bitrate     = 500 * 1000;
+        int32_t job_stretch_mode    = 0;
+
+        std::string watermark_url   = "http://mars-assets.qnssl.com/qiniulogo/normal-logo-blue.png";
+        int32_t watermark_x         = 0;
+        int32_t watermark_y         = 0;
+        int32_t watermark_width     = 80;
+        int32_t watermark_height    = 80;
+
+        std::string background_url  = "http://mars-assets.qnssl.com/qiniulog/img-slogan-white-en.png";
+        int32_t background_x        = 0;
+        int32_t background_y        = 0;
+        int32_t background_width    = 300;
+        int32_t background_height   = 100;
+
+        bool merge_local_video      = true;
+        bool merge_local_audio      = true;
+        int32_t local_video_x       = 0;
+        int32_t local_video_y       = 0;
+        int32_t local_video_width   = 200;
+        int32_t local_video_height  = 200;
+
+        bool merge_remote_video     = true;
+        bool merge_remote_audio     = true;
+        int32_t remote_video_x      = 200;
+        int32_t remote_video_y      = 200;
+        int32_t remote_video_width  = 200;
+        int32_t remote_video_height = 200;
+
+    } _merge_config;
+
+    #define MERGE_MESSAGE_ID    100
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_MERGE };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+    virtual BOOL OnInitDialog();
+
+	DECLARE_MESSAGE_MAP()
+};
